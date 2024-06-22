bf31e120a293222bf4e180f9402fe5cdb1e0e8c9
jayaprakashponnala <jayaprakashponnala13@gmail.com>
2024-06-22 19:28:49 +0530

    final code

---
+++ script.js
@@ -0,0 +1,12 @@
+//   typing  Animation 
+var typed  = new Typed(".typing",{
+    strings:["FullStack  Developer","Web Developer","Backend Developer","Technical content Writer"],
+    typespeed:100000,
+    Backspeed:50000,
+    loop:true
+}) 
+
+ 
+
+
+ 
