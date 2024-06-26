082bbe16c6511b527f6c3035e5aa2d7aaac8d3cc
jayaprakashponnala <jayaprakashponnala13@gmail.com>
2024-06-27 03:42:00 +0530

    final code

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 //   typing  Animation 
 var typed  = new Typed(".typing",{
     strings:["FullStack  Developer","Web Developer","Backend Developer","Technical content Writer"],
-    typespeed:100000,
-    Backspeed:50000,
+    typeSpeed:50,
+    backSpeed: 50,
+    startDelay:1000,
     loop:true
 }) 
+
+ // Add an event listener for all anchor tags inside the nav
+ document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function() {
+        // Remove the "active" class from all links
+        document.querySelectorAll('nav ul li a').forEach(item => {
+            item.classList.remove('active');
+        });
+        // Add the "active" class to the clicked link
+        this.classList.add('active');
+    });
+});
 
  
 
